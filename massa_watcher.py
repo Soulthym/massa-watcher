29eb87260979a6912a742c37d35dbad43f90696b
Thybault Alabarbe <thybault.alabarbe@gmail.com>
2025-06-14 16:39:20 +0200

    watch command + save and load state ftom csv

--- massa_watcher.py
+++ massa_watcher.py
@@ -5,6 +5,34 @@ from env import command
 from env import TG_USERNAME
 from env import TG_ADMIN
 
+def read_csv(file_path):
+    """Read a CSV file and return a list of dictionaries."""
+    import csv
+    with file_path.open("r") as f:
+        reader = csv.DictReader(f)
+        res = {}
+        for row in reader:
+            key = int(row["user"])
+            if key not in res:
+                res[key] = []
+            res[key].append(row["address"])
+        return res
+
+def write_csv(file_path, data):
+    """Write a list of dictionaries to a CSV file."""
+    import csv
+    with file_path.open("w", newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=["user", "address"])
+        writer.writeheader()
+        for user, addresses in data.items():
+            for address in addresses:
+                writer.writerow({"user": int(user), "address": address})
+
+watching_file = data_dir / "watching.csv"
+watching: dict[int, list[str]] = read_csv(watching_file)
+if not watching_file.exists():
+    write_csv(watching_file, {})
+
 def get_name(user, prefix=""):
     name_parts = []
     if getattr(user, "first_name", None):
@@ -34,17 +62,26 @@ async def start(event, cmd):
     await event.reply("\n".join(msg))
 
 address_pat = r"AU[1-9A-HJ-NP-Za-hj-np-z]{51}"
-@command(cmd="watch", address=address_pat)
+@command(address=address_pat)
 async def watch(event, address):
     # Here you would add the logic to start watching the address
+    user = event.sender_id
+    if user not in watching:
+        watching[user] = []
+    if address in watching[user]:
+        return await event.reply(f"You are already watching address: {address}")
+    watching[user].append(address)
     await event.reply(f"Started watching address: {address}")
 
 async def main():
     print("Connected to Telegram as", TG_USERNAME)
-    async with massa_node():
-        print("Massa node is running. Press Ctrl+C to stop.")
-        await bot.send_message(TG_ADMIN, f"Bot started successfully as {TG_USERNAME}.")
-        await bot.run_until_disconnected()
+    try:
+        async with massa_node():
+            print("Massa node is running. Press Ctrl+C to stop.")
+            await bot.send_message(TG_ADMIN, f"Bot started successfully as {TG_USERNAME}.")
+            await bot.run_until_disconnected()  # type: ignore
+    except Exception as e:
+        write_csv(watching_file, watching)
 
 if __name__ == "__main__":
     with bot:
